479eab74a8a15f03d62fc4a0b0045969fdbd8118
Vivien <vivien.tran-thien_git@m4x.org>
2021-02-13 23:13:14 +0100

    Add video and possibility to hide video & dropdown

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
-import { World } from './World/World.js';
+import {
+  World
+} from './World/World.js';
 
-const parameter = (location.search.split('object=')[1]||'').split('&')[0]
+const parameter = (location.search.split('object=')[1] || '').split('&')[0]
 const objectID = parameter ? parameter : 0;
+const parameter2 = (location.search.split('controls=')[1] || '').split('&')[0]
+const displayControls = parameter2 ? parameter2 : 1;
 
 async function main() {
 
   const selector = document.querySelector('#object');
   selector.value = objectID;
 
+  if (displayControls != 0) {
+    document.querySelector('#controls').style.visibility = "visible";
+  }
+
+  document.onkeypress = function (e) {
+    var evt = window.event || e;
+    switch (evt.keyCode) {
+      case 99:
+        if (document.querySelector('#controls').style.visibility == "visible") {
+          document.querySelector('#controls').style.visibility = "hidden";
+        } else {
+          document.querySelector('#controls').style.visibility = "visible";
+        }
+        break;
+    }
+  }
   // Get a reference to the container element 
   const container = document.querySelector('#scene-container');
 
@@ -24,9 +44,9 @@ async function main() {
 
   selector.addEventListener('change', async (event) => {
     world.stop();
-    while(container.firstChild) { 
-      container.removeChild(container.firstChild); 
-    } 
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
     world = new World(container);
     await world.init();
     world.start();
